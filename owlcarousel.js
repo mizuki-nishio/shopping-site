d30a9e88c43eb3c8da89f3243df4fa34e0c0d1e9
mizukinishio <hu1109ni@gmail.com>
2017-03-29 06:59:03 +0000

    first commit

---
+++ owlcarousel.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+$(document).ready(function(){
+  $(".menu_sp").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".menu").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
